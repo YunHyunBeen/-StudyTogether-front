9ee2754c75c5a4d14042edd410f31753a5e41f83
YunHyunBeen <nsntks19@naver.com>
2019-07-30 14:40:41 +0900

    헷

---
+++ src/MyPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyPage extends Component {
+    render() {
+        return(
+            <div>
+                여긴 마이페이지 입니다.
+            </div>
+        );
+    }
+}
+
+export default MyPage;
